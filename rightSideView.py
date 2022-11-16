eefd087a1da84279b7ff8c79d69a7c427a6d9660
wsl <wslzzy@qq.com>
2022-11-17 00:17:12 +0800

    rightSideView

--- rightSideView.py
+++ rightSideView.py
@@ -9,9 +9,19 @@ def rightSideView(root: TreeNode):
         q.append(root)
     else:
         return res
-
-
+    while q:
+        size = len(q)
+        for i in range(size):
+            node = q.popleft()
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+            if i == size - 1:
+                res.append(node.value)
+    return res
 if __name__ == '__main__':
     root = TreeNode(27)
     Solution.doInsert(root)
     print(Solution.bfs(root))
+    print(rightSideView(root))
